9c65ea9c0e9cabc687eb584e8e4cc47e07530437
Nikita Grebennikov <nikitagreb0204@mail.ru>
2021-10-16 11:28:00 +0300

    13.121

---
+++ src/components/Search.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export const Search = () => {
+
+  return (
+    <div className="form-group pb-3">
+      <input
+        type="text"
+        className="form-control"
+        placeholder="Введите ник пользователя"
+      />
+    </div>
+  )
+}
+
