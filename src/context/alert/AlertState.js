c48845cac2cce584f444a6f0aec41d55b115cc13
Nikita Grebennikov <nikitagreb0204@mail.ru>
2021-10-19 08:56:13 +0300

    14.128

--- src/context/alert/AlertState.js
+++ src/context/alert/AlertState.js
@@ -1,5 +1,5 @@
 import React, {useReducer} from 'react'
-import {AlertContext} from './AlertContext'
+import {AlertContext} from './alertContext'
 import {alertReducer} from './alertReducer'
 import {ALERT_HIDE, ALERT_SHOW} from '../types'
 
